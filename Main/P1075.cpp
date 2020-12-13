bca107a07d489c5c4f451e4ae0e372f4fbf1c099
B-1168 <40647091+qinyihao@users.noreply.github.com>
2020-12-13 10:39:07 +0800

    Create P1075.cpp

---
+++ Main/P1075.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+int main()
+{
+    scanf("%d", &n);
+    for(int i = 2;i < n;i++)
+    {
+        if(n % i == 0)
+        {
+            printf("%d\n", n/i);
+            return 0;
+        }
+    }
+    return 0;
+}
