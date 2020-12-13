8fa160de701c93c0bfcabb62d2de0fcba570e4f0
B-1168 <40647091+qinyihao@users.noreply.github.com>
2020-12-13 11:02:51 +0800

    Create CF1372A.cpp

---
+++ Codeforces/CF1372A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+int32_t main() {
+	int t;
+	cin >> t;
+	while(t--) {
+		int n;
+		cin >> n;
+		for(int i = 0; i < n; i++) {
+			cout << "1 ";
+		}
+		cout << '\n';
+	}
+}
