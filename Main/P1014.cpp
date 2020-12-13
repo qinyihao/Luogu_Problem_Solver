9ff4e89c76aa5b38cc860aab7521a66184daafd1
B-1168 <40647091+qinyihao@users.noreply.github.com>
2020-12-13 10:27:58 +0800

    Create P1014.cpp

---
+++ Main/P1014.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long l=1,r,mid,n,a;
+    cin>>n;
+    r=n;
+    while(l<r)
+    {
+        mid=(l+r)/2;
+        if(mid*(mid+1)/2<n)l=mid+1;
+        else r=mid;
+    }
+    a=n-l*(l-1)/2;
+    if(l%2==0)cout<<a<<'/'<<l+1-a;
+    else cout<<l+1-a<<'/'<<a;
+    return 0;
+}
