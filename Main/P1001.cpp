7247329eda0448b10717433093488df1ec4d3c40
B-1168 <40647091+qinyihao@users.noreply.github.com>
2020-12-13 10:20:12 +0800

    Create P1001.cpp

---
+++ Main/P1001.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+
+int main() {
+    ull a,b;
+    cin >> a >> b;
+    cout << a + b;
+    return 0;
+}
